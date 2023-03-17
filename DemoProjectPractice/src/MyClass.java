b97bc227838c9a97b6385b7696a1b8d6a74ce834
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2023-03-17 16:36:53 -0700

    Basic Task

---
+++ DemoProjectPractice/src/MyClass.java
@@ -0,0 +1,14 @@
+
+public class MyClass 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test base");
+		System.out.println("pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resources");
+		System.out.println("Suite");
+
+	}
+
+}
